61b2b67ca2b3812d4b7a0cc5114fac3064ce992b
Lawe Zangena <lawza02@gmail.com>
2021-10-20 13:04:41 +0200

    Lawe ready to go

--- router/app.js
+++ router/app.js
@@ -3,14 +3,10 @@ const router = express.Router();
 
 const app_Controller = require('../controller/appController');
 
-<<<<<<< HEAD
-router.get('/', app_Controller.app);
-router.get('/bok', app_Controller.bok),
-
-=======
 router.get('/', app_Controller.login);
 router.get('/booking', app_Controller.app);
->>>>>>> 90d5f1032b2286bc9fabaeb056b907f73ef93e1b
+router.get('/myBookings', app_Controller.myBookings);
+
 
 
 
