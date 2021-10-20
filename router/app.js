f43faa98776f9edf7f70f3877cbc2c279110a87c
vigor-lundkvist <vigor.lundkvist@live.se>
2021-10-20 12:20:12 +0200

    Added necessary files, and used prior login function.

--- router/app.js
+++ router/app.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const app_Controller = require('../controller/appController');
 
-router.get('/', app_Controller.app);
+router.get('/', app_Controller.login);
+router.get('/booking', app_Controller.app);
 
 
 
