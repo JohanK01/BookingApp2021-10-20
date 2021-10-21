893d46a32dc653facc314a6e6002f172f41f3b38
JohanK <klausenjohan23@gmail.com>
2021-10-21 08:40:24 +0200

    did a input box and added a button and json

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,20 @@
-myBookFunction() = {
-    
+'use strict';
+let xhttp = new XMLHttpRequest;
+xhttp.open('GET', '../data/user.json');
+xhttp.responseType = 'json';
+xhttp.send();
+
+xhttp.onload = () => {
+    const data = xhttp.response;
+    console.log(data);
+    const pelikan = data['users']
+
+    let div1 = document.createElement('p');
+    div1.textContent = (pelikan.FirstName);
+
+   document.getElementById('anka').appendChild(div1);
+};
+function myBookFunction() {
+    document.getElementById('mybtn').innerHTML = "Bokat!";
 
 };
